e5c072d42012958a49e78a3b7503d198ce1d3c29
Yousef Abusamhadana <abusay6@muhs.edu>
2024-02-23 19:05:50 -0600

    commands for climber

---
+++ src/main/java/frc/robot/Commands/climber/Retract.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands.climber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.climber.Climber;
+
+
+public class Retract extends Command {
+  private final Climber climber;
+  /** Creates a new Retract. */
+  public Retract(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
+   this.climber = climber;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    climber.climberRetract();
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climber.stopClimber();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
